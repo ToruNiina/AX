5962a9fd6d5df6585de0684d1362fe6f729289b8
ToruNiina <niina.toru.68u@gmail.com>
2016-04-04 19:56:03 +0900

    add dynamic class tag

--- Expression.hpp
+++ Expression.hpp
@@ -12,6 +12,11 @@ namespace ax
     class Vector{};
     class VectorExp{};
 
+    class DynamicMatrix{};
+    class DynamicMatrixExp{};
+    class DynamicVector{};
+    class DynamicVectorExp{};
+
     template <typename T>
     class is_MatrixExpression:public std::false_type{};
     template <>
@@ -35,6 +40,21 @@ namespace ax
     class is_ScalarType: public std::false_type{};
     template <>
     class is_ScalarType<double>: public std::true_type{};
+
+    // =============================== dynamic =================================
+    template <typename T>
+    class is_DynamicVectorExpression:public std::false_type{};
+    template <>
+    class is_DynamicVectorExpression<Vector>:public std::true_type{};
+    template <>
+    class is_DynamicVectorExpression<VectorExp>:public std::true_type{};
+
+    template <typename T>
+    class is_DynamicMatrixExpression:public std::false_type{};
+    template <>
+    class is_DynamicMatrixExpression<DynamicMatrix>:public std::true_type{};
+    template <>
+    class is_DynamicMatrixExpression<DynamicMatrixExp>: public std::true_type{};
 
 }
 #endif //AX_MATRIX_EXPRESSION_H
