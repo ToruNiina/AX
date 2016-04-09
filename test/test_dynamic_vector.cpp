f3dc07b46ade3f4b82f5396959383b7791fb7279
ToruNiina <niina.toru.68u@gmail.com>
2016-04-09 17:59:42 +0900

    add test of dynamic vector

---
+++ test/test_dynamic_vector.cpp
@@ -0,0 +1,186 @@
+#define BOOST_TEST_MODULE "test_dynamic_vector"
+
+#ifdef UNITTEST_FRAMEWORK_LIBRARY_EXIST
+#include <boost/test/unit_test.hpp>
+#else
+#define BOOST_TEST_NO_LIB
+#include <boost/test/included/unit_test.hpp>
+#endif
+
+#include "DynamicVector.hpp"
+using VectorDd = ax::RealDynamicVector;
+
+#include "test_Defs.hpp"
+using ax::test::seed;
+
+#include <random>
+
+BOOST_AUTO_TEST_CASE(VectorNd_Constructable)
+{
+    const VectorDd vec(10);
+
+    for(std::size_t i=0; i<10; ++i)
+        BOOST_CHECK_EQUAL(vec[i], 0e0);
+
+    const VectorDd vec_1(10, 1e0);
+
+    for(std::size_t i=0; i<10; ++i)
+        BOOST_CHECK_EQUAL(vec_1[i], 1e0);
+
+    const VectorDd vec_cp_0(vec_1);
+
+    for(std::size_t i=0; i<10; ++i)
+        BOOST_CHECK_EQUAL(vec_cp_0[i], 1e0);
+
+    const VectorDd vec_cp_1 = vec_1;
+
+    for(std::size_t i=0; i<10; ++i)
+        BOOST_CHECK_EQUAL(vec_cp_1[i], 1e0);
+}
+
+BOOST_AUTO_TEST_CASE(VectorNd_Add)
+{
+    const VectorDd vec1(10, 1e0);
+    const VectorDd vec2(10, 2e0);
+    const VectorDd vec3(vec1 + vec2);
+
+    for(std::size_t i=0; i<10; ++i)
+        BOOST_CHECK_EQUAL(vec3[i], 3e0);
+
+    VectorDd vec4;
+    vec4 = vec1 + vec2;
+
+    for(std::size_t i=0; i<10; ++i)
+        BOOST_CHECK_EQUAL(vec4[i], 3e0);
+
+    VectorDd vec5(10);
+    vec5 += vec1;
+    vec5 += vec2;
+
+    for(std::size_t i=0; i<10; ++i)
+        BOOST_CHECK_EQUAL(vec5[i], 3e0);
+}
+
+BOOST_AUTO_TEST_CASE(VectorNd_Sub)
+{
+    const VectorDd vec1(10, 3e0);
+    const VectorDd vec2(10, 2e0);
+    const VectorDd vec3(vec1 - vec2);
+
+    for(std::size_t i=0; i<10; ++i)
+        BOOST_CHECK_EQUAL(vec3[i], 1e0);
+
+    VectorDd vec4;
+    vec4 = vec1 - vec2;
+
+    for(std::size_t i=0; i<10; ++i)
+        BOOST_CHECK_EQUAL(vec4[i], 1e0);
+
+    VectorDd vec5(vec1);
+    vec5 -= vec2;
+
+    for(std::size_t i=0; i<10; ++i)
+        BOOST_CHECK_EQUAL(vec5[i], 1e0);
+}
+
+BOOST_AUTO_TEST_CASE(VectorNd_Scalar_multiple)
+{
+    const VectorDd vec1(10, 1e0);
+    const VectorDd vec2(2e0 * vec1);
+
+    for(std::size_t i=0; i<10; ++i)
+        BOOST_CHECK_EQUAL(vec2[i], 2e0);
+
+    const VectorDd vec3(vec1 * 2e0);
+
+    for(std::size_t i=0; i<10; ++i)
+        BOOST_CHECK_EQUAL(vec3[i], 2e0);
+
+    VectorDd vec4;
+    vec4 = vec1 * 2e0;
+
+    for(std::size_t i=0; i<10; ++i)
+        BOOST_CHECK_EQUAL(vec4[i], 2e0);
+
+    vec4 = 2e0 * vec1;
+
+    for(std::size_t i=0; i<10; ++i)
+        BOOST_CHECK_EQUAL(vec4[i], 2e0);
+
+    VectorDd vec5(vec1);
+    vec5 *= 2e0;
+
+    for(std::size_t i=0; i<10; ++i)
+        BOOST_CHECK_EQUAL(vec5[i], 2e0);
+}
+
+BOOST_AUTO_TEST_CASE(VectorNd_Scalar_division)
+{
+    const VectorDd vec1(10, 2e0);
+    const VectorDd vec2(vec1 / 2e0);
+
+    for(std::size_t i=0; i<10; ++i)
+        BOOST_CHECK_EQUAL(vec2[i], 1e0);
+
+    VectorDd vec3;
+    vec3 = vec1 / 2e0;
+
+    for(std::size_t i=0; i<10; ++i)
+        BOOST_CHECK_EQUAL(vec3[i], 1e0);
+
+    VectorDd vec4(vec1);
+    vec4 /= 2e0;
+
+    for(std::size_t i=0; i<10; ++i)
+        BOOST_CHECK_EQUAL(vec4[i], 1e0);
+}
+
+BOOST_AUTO_TEST_CASE(VectorNd_dot_product)
+{
+    std::mt19937 mt(seed);
+    std::uniform_real_distribution<double> randreal(0e0, 1e0);
+
+    for(auto i=0; i<100; ++i)
+    {
+        std::vector<double> v1(10);
+        for(std::size_t i=0; i<10; ++i)
+            v1.at(i) = randreal(mt);
+
+        std::vector<double> v2(10);
+        for(std::size_t i=0; i<10; ++i)
+            v2.at(i) = randreal(mt);
+
+        const VectorDd vec1(v1);
+        const VectorDd vec2(v2);
+
+        double dot_product = 0e0;
+        for(std::size_t i=0; i<10; ++i)
+            dot_product += v1.at(i) * v2.at(i);
+
+        BOOST_CHECK_EQUAL(dot_prod(vec1, vec2), dot_product);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(VectorNd_length)
+{
+    std::mt19937 mt(seed);
+    std::uniform_real_distribution<double> randreal(0e0, 1e0);
+
+    for(auto i=0; i<100; ++i)
+    {
+        std::array<double, 10> v1;
+        for(std::size_t i=0; i<10; ++i)
+            v1.at(i) = randreal(mt);
+
+        const VectorDd vec1(v1);
+
+        double lensq = 0e0;
+        for(std::size_t i=0; i<10; ++i)
+            lensq += v1.at(i) * v1.at(i);
+
+        BOOST_CHECK_EQUAL(len_square(vec1), lensq);
+        BOOST_CHECK_EQUAL(length(vec1), std::sqrt(lensq));
+
+        BOOST_CHECK_EQUAL(len_square(vec1), dot_prod(vec1, vec1));
+    }
+}
