e6d2c2f468e23ea5f5ed6bd14c3cded82e31bc77
ToruNiina <niina.toru.68u@gmail.com>
2016-04-12 10:13:51 +0900

    add small-size inverse matrix

---
+++ src/InverseMatrix.hpp
@@ -0,0 +1,79 @@
+#ifndef AX_INVERSE_MATRIX_H
+#define AX_INVERSE_MATRIX_H
+#include "Matrix.hpp"
+
+namespace ax
+{
+
+// for 2x2 dimentional
+template<class L, typename std::enable_if<
+             is_MatrixExpression<typename L::value_trait>::value&&
+             is_SameSize<L::col, L::row>::value&&
+             is_SameSize<L::col, 2>::value
+             >::type*& = enabler>
+const double determinant(const L& lhs)
+{
+    return lhs(0,0) * lhs(1,1) - lhs(1,0) * lhs(0,1);
+}
+
+// for 3x3 dimentional
+template<class L, typename std::enable_if<
+             is_MatrixExpression<typename L::value_trait>::value&&
+             is_SameSize<L::col, L::row>::value&&
+             is_SameSize<L::col, 3>::value
+             >::type*& = enabler>
+const double determinant(const L& lhs)
+{
+    return lhs(0,0) * lhs(1,1) * lhs(2,2) +
+           lhs(1,0) * lhs(2,1) * lhs(0,2) +
+           lhs(2,0) * lhs(0,1) * lhs(1,2) - 
+           lhs(0,0) * lhs(2,1) * lhs(1,2) - 
+           lhs(2,0) * lhs(1,1) * lhs(0,2) - 
+           lhs(1,0) * lhs(0,1) * lhs(2,2);
+}
+
+// XXX: for 2x2 matrix, cost of temporal object is small
+template<class L, typename std::enable_if<
+             is_MatrixExpression<typename L::value_trait>::value&&
+             is_SameSize<L::col, L::row>::value&&
+             is_SameSize<L::col, 2>::value
+             >::type*& = enabler>
+RealMatrix<L::row, L::col> inverse(const L& lhs)
+{
+    const double det_inv = 1e0 / determinant(lhs);
+    RealMatrix<L::row, L::col> inv;
+    inv(0,0) =  det_inv * lhs(1,1);
+    inv(1,1) =  det_inv * lhs(0,0);
+    inv(0,1) = -det_inv * lhs(0,1);
+    inv(1,0) = -det_inv * lhs(1,0);
+    return inv;
+}
+
+// XXX: for 3x3 matrix, cost of temporal object is small
+template<class L, typename std::enable_if<
+             is_MatrixExpression<typename L::value_trait>::value&&
+             is_SameSize<L::col, L::row>::value&&
+             is_SameSize<L::col, 3>::value
+             >::type*& = enabler>
+RealMatrix<L::row, L::col> inverse(const L& lhs_)
+{
+    const L lhs = lhs_;
+    const double det_inv = 1e0 / determinant(lhs);
+
+    RealMatrix<L::row, L::col> inv;
+    inv(0,0) = det_inv * (lhs(1,1) * lhs(2,2) - lhs(1,2) * lhs(2,1));
+    inv(1,1) = det_inv * (lhs(0,0) * lhs(2,2) - lhs(0,2) * lhs(2,0));
+    inv(2,2) = det_inv * (lhs(0,0) * lhs(1,1) - lhs(0,1) * lhs(1,0));
+
+    inv(0,1) = det_inv * (lhs(0,2) * lhs(2,1) - lhs(0,1) * lhs(2,2));
+    inv(0,2) = det_inv * (lhs(0,1) * lhs(1,2) - lhs(0,2) * lhs(1,1));
+    inv(1,2) = det_inv * (lhs(0,2) * lhs(1,0) - lhs(0,0) * lhs(1,2));
+
+    inv(1,0) = det_inv * (lhs(1,2) * lhs(2,0) - lhs(1,0) * lhs(2,2));
+    inv(2,0) = det_inv * (lhs(1,0) * lhs(2,1) - lhs(2,0) * lhs(1,1));
+    inv(2,1) = det_inv * (lhs(2,0) * lhs(0,1) - lhs(0,0) * lhs(2,1));
+    return inv;
+}
+
+}
+#endif /* AX_INVERSE_MATRIX_H */
---
+++ test/test_2or3_inverse_matrix.cpp
@@ -0,0 +1,79 @@
+#define BOOST_TEST_MODULE "test_small_inverse_matrix"
+
+#ifdef UNITTEST_FRAMEWORK_LIBRARY_EXIST
+#include <boost/test/unit_test.hpp>
+#else
+#define BOOST_TEST_NO_LIB
+#include <boost/test/included/unit_test.hpp>
+#endif
+
+#include "InverseMatrix.hpp"
+
+#include "test_Defs.hpp"
+using ax::test::tolerance;
+using ax::test::seed;
+
+#include <random>
+
+
+BOOST_AUTO_TEST_CASE(matrix_2x2)
+{
+    constexpr std::size_t msize = 2;
+
+    std::mt19937 mt(seed);
+    std::uniform_real_distribution<double> randreal(0e0, 1e0);
+
+    ax::RealMatrix<msize,msize> mat;
+    double det = 0e0;
+
+    while(det == 0e0)
+    {
+        std::array<std::array<double, msize>, msize> rand1;
+        for(std::size_t i=0; i<msize; ++i)
+            for(std::size_t j=0; j<msize; ++j)
+                mat(i,j) = rand1[i][j] = randreal(mt);
+
+        det = ax::determinant(mat);
+    }
+
+    const ax::RealMatrix<msize,msize> inv = inverse(mat);
+    const ax::RealMatrix<msize,msize> E   = inv * mat;
+
+    for(std::size_t i=0; i<msize; ++i)
+        for(std::size_t j=0; j<msize; ++j)
+            if(i==j)
+                BOOST_CHECK_CLOSE(E(i,j), 1e0, tolerance);
+            else
+                BOOST_CHECK_SMALL(E(i,j), tolerance);
+}
+
+BOOST_AUTO_TEST_CASE(matrix_3x3)
+{
+    constexpr std::size_t msize = 3;
+
+    std::mt19937 mt(seed);
+    std::uniform_real_distribution<double> randreal(0e0, 1e0);
+
+    ax::RealMatrix<msize,msize> mat;
+    double det = 0e0;
+
+    while(det == 0e0)
+    {
+        std::array<std::array<double, msize>, msize> rand1;
+        for(std::size_t i=0; i<msize; ++i)
+            for(std::size_t j=0; j<msize; ++j)
+                mat(i,j) = rand1[i][j] = randreal(mt);
+
+        det = ax::determinant(mat);
+    }
+
+    const ax::RealMatrix<msize,msize> inv = inverse(mat);
+    const ax::RealMatrix<msize,msize> E   = inv * mat;
+
+    for(std::size_t i=0; i<msize; ++i)
+        for(std::size_t j=0; j<msize; ++j)
+            if(i==j)
+                BOOST_CHECK_CLOSE(E(i,j), 1e0, tolerance);
+            else
+                BOOST_CHECK_SMALL(E(i,j), tolerance);
+}
