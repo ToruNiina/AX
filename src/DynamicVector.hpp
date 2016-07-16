ef21d2d3bfc81c8a0446201cdc67980369f7df79
ToruNiina <niina.toru.68u@gmail.com>
2016-07-16 13:39:33 +0900

    change definition ofDynamic Vector

--- src/DynamicVector.hpp
+++ src/DynamicVector.hpp
@@ -8,231 +8,174 @@
 namespace ax
 {
 
-class RealDynamicVector
+template<typename T_elem, int I_dim,
+         typename std::enable_if<is_dynamic_dimention<I_dim>::value
+             >::type*& = enabler>
+class Vector
 {
   public:
-    using value_trait = DynamicVector;
+
+    using tag = vector_tag;
+    using elem_t = T_elem;
+    constexpr static std::size_t dim = I_dim;
 
   public:
-    RealDynamicVector(){}
-    ~RealDynamicVector() = default;
+    Vector(){}
+    ~Vector() = default;
 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ctor ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    RealDynamicVector(const std::size_t size)
-        : values_(size, 0e0)
-    {}
-
-    RealDynamicVector(const std::size_t size, const double v)
-        : values_(size, v)
-    {}
-
-    RealDynamicVector(const std::vector<double>& v)
-        : values_(v)
-    {}
-
-    RealDynamicVector(std::vector<double>&& v)
-        : values_(std::move(v))
-    {}
+    Vector(const std::size_t size) : values_(size, 0e0){}
+    Vector(const std::size_t size, const double v) : values_(size, v){}
+    Vector(const std::vector<double>& v) : values_(v){}
+    Vector(std::vector<double>&& v): values_(std::move(v)){}
 
     template<std::size_t N>
-    RealDynamicVector(const std::array<double, N>& v)
-        : values_(N, 0e0)
+    Vector(const std::array<double, N>& v): values_(N)
     {
-        for(std::size_t i=0; i<N; ++i)
-            values_[i] = v[i];
+        for(std::size_t i=0; i<N; ++i) values_[i] = v[i];
     }
 
-    RealDynamicVector(const RealDynamicVector& vec)
-        : values_(vec.values_)
-    {}
+    // from dynamic Vector
+    Vector(const Vector<elem_t, dim>& vec) : values_(vec.values_){}
+    Vector(Vector<elem_t, dim>&& vec) : values_(std::move(vec.values_)){}
 
-    RealDynamicVector(RealDynamicVector&& vec)
-        : values_(std::move(vec.values_))
-    {}
-
-    template<class E,
-            typename std::enable_if<
-                is_VectorExpression<typename E::value_trait>::value
-                >::type*& = enabler>
-    RealDynamicVector(const E& vec)
+    // from static Vector
+    template<int D, typename std::enable_if<
+        is_static_dimention<D>::value>::type*& = enabler>
+    Vector(const Vector<elem_t, D>& vec) : values_(D)
     {
-//         this->values_.clear();
-        this->values_.resize(E::size, 0e0);
-        for(std::size_t i=0; i<E::size; ++i)
-            this->values_.at(i) = vec[i];
+        for(std::size_t i=0; i<D; ++i) this->values_[i] = vec[i];
     }
 
-    template<class E,
-            typename std::enable_if<
-                is_DynamicVectorExpression<typename E::value_trait>::value
-                >::type*& = enabler>
-    RealDynamicVector(const E& vec)
+    // from dynamic Vector expression
+    template<class T_expr, typename std::enable_if<
+                is_vector_expression<typename T_expr::tag>::value&&
+                is_dynamic_dimention<T_expr::dim>::value>::type*& = enabler>
+    Vector(const T_expr& expr) : values_(expr.size())
     {
-//         this->values_.clear();
-        this->values_.resize(vec.size(), 0e0);
-        for(std::size_t i=0; i<vec.size(); ++i)
-            this->values_.at(i) = vec[i];
+        for(std::size_t i=0; i<expr.size(); ++i) this->values_[i] = expr[i];
+    }
+
+    // from static Vector
+    template<class T_expr, typename std::enable_if<
+                is_vector_expression<typename T_expr::tag>::value&&
+                is_static_dimention<T_expr::dim>::value>::type*& = enabler>
+    Vector(const T_expr& expr) : values_(T_expr::dim)
+    {
+        for(std::size_t i=0; i<T_expr::dim; ++i) this->values_[i] = expr[i];
     }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ operator = ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    RealDynamicVector& operator=(const std::vector<double>& v)
+    Vector<elem_t, dim>& operator=(const std::vector<double>& v)
     {
-        this->values_ = v;
-        return *this;
+        this->values_ = v; return *this;
     }
 
-    RealDynamicVector& operator=(std::vector<double>&& v)
+    Vector<elem_t, dim>& operator=(std::vector<double>&& v)
     {
-        this->values_ = std::move(v);
-        return *this;
+        this->values_ = std::move(v); return *this;
     }
 
-    RealDynamicVector& operator=(const RealDynamicVector& vec)
+    Vector<elem_t, dim>& operator=(const Vector<elem_t, dim>& vec)
     {
-        this->values_ = vec.values_;
-        return *this;
+        this->values_ = vec.values_; return *this;
     }
 
-
-    RealDynamicVector& operator=(RealDynamicVector&& vec)
+    Vector<elem_t, dim>& operator=(Vector<elem_t, dim>&& vec)
     {
-        this->values_ = std::move(vec.values_);
-        return *this;
+        this->values_ = std::move(vec.values_);return *this;
     }
 
-    template<class E,
+    template<class T_expr,
             typename std::enable_if<
-                is_VectorExpression<typename E::value_trait>::value
-                >::type*& = enabler>
-    RealDynamicVector& operator=(const E& vec)
+                is_vector_expression<typename T_expr::tag>::value&&
+                is_dynamic_dimention<T_expr::dim>::value>::type*& = enabler>
+    Vector<elem_t, dim>& operator=(const T_expr& expr)
     {
-//         this->values_.clear();
-        this->values_.resize(E::size, 0e0);
-        for(std::size_t i=0; i<E::size; ++i)
-            this->values_.at(i) = vec[i];
+        this->values_.resize(expr.size(), 0e0);
+        for(std::size_t i=0; i<expr.size(); ++i) this->values_[i] = expr[i];
         return *this;
     }
 
-    template<class E,
+    template<class T_expr,
             typename std::enable_if<
-                is_DynamicVectorExpression<typename E::value_trait>::value
-                >::type*& = enabler>
-    RealDynamicVector& operator=(const E& vec)
+                is_vector_expression<typename T_expr::tag>::value&&
+                is_static_dimention<T_expr::dim>::value>::type*& = enabler>
+    Vector<elem_t, dim>& operator=(const T_expr& expr)
     {
-//         this->values_.clear();
-        this->values_.resize(vec.size(), 0e0);
-        for(std::size_t i=0; i<vec.size(); ++i)
-            this->values_.at(i) = vec[i];
+        this->values_.resize(T_expr::dim, 0e0);
+        for(std::size_t i=0; i<T_expr::dim; ++i) this->values_[i] = expr[i];
         return *this;
     }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ operator += ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // for dynamic += dynamic
-    template<class E, 
-             typename std::enable_if<
-                     is_DynamicVectorExpression<typename E::value_trait>::value
-                 >::type*& = enabler>
-    RealDynamicVector& operator+=(const E& exp)
+    template<class T_expr,
+            typename std::enable_if<
+                is_vector_expression<typename T_expr::tag>::value&&
+                is_dynamic_dimention<T_expr::dim>::value>::type*& = enabler>
+    Vector<elem_t, dim>& operator+=(const T_expr& expr)
     {
-        if(this->size() != exp.size())
+        if(this->size() != expr.size())
             throw std::invalid_argument("add different size vector");
-
-        *this = DynamicVectorAdd<RealDynamicVector, E>(*this, exp);
-        return *this;
+        return *this = (*this + expr);
     } 
 
     // for dynamic += static
-    template<class E, 
-             typename std::enable_if<
-                     is_VectorExpression<typename E::value_trait>::value
-                 >::type*& = enabler>
-    RealDynamicVector& operator+=(const E& exp)
+    template<class T_expr,
+            typename std::enable_if<
+                is_vector_expression<typename T_expr::tag>::value&&
+                is_static_dimention<T_expr::dim>::value>::type*& = enabler>
+    Vector<elem_t, dim>& operator+=(const T_expr& expr)
     {
-        if(this->size() != E::size)
+        if(this->size() != T_expr::dim)
             throw std::invalid_argument("add different size vector");
-
-        *this = DynamicVectorAdd<RealDynamicVector, E>(*this, exp);
-        return *this;
-    } 
+        return *this = (*this + expr);
+    }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ operator -= ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // for dynamic -= dynamic
-    template<class E, 
-             typename std::enable_if<
-                     is_DynamicVectorExpression<typename E::value_trait>::value
-                 >::type*& = enabler>
-    RealDynamicVector& operator-=(const E& exp)
+    template<class T_expr,
+            typename std::enable_if<
+                is_vector_expression<typename T_expr::tag>::value&&
+                is_dynamic_dimention<T_expr::dim>::value>::type*& = enabler>
+    Vector<elem_t, dim>& operator-=(const T_expr& expr)
     {
-        if(this->size() != exp.size())
+        if(this->size() != expr.size())
             throw std::invalid_argument("add different size vector");
-
-        *this = DynamicVectorSub<RealDynamicVector, E>(*this, exp);
-        return *this;
+        return *this = (*this - expr);
     }
 
     // for dynamic -= static
-    template<class E, 
-             typename std::enable_if<
-                     is_VectorExpression<typename E::value_trait>::value
-                 >::type*& = enabler>
-    RealDynamicVector& operator-=(const E& exp)
+    template<class T_expr,
+            typename std::enable_if<
+                is_vector_expression<typename T_expr::tag>::value&&
+                is_static_dimention<T_expr::dim>::value>::type*& = enabler>
+    Vector<elem_t, dim>& operator-=(const T_expr& expr)
     {
-        if(this->size() != E::size)
-            throw std::invalid_argument("subtract different size vector");
+        if(this->size() != T_expr::dim)
+            throw std::invalid_argument("add different size vector");
+        return *this = (*this - expr);
+    }
 
-        *this = DynamicVectorSub<RealDynamicVector, E>(*this, exp);
-        return *this;
+    Vector<elem_t, dim>& operator*=(const elem_t& scl)
+    {
+        return *this = (*this * scl);
+    } 
+    Vector<elem_t, dim>& operator/=(const elem_t& scl)
+    {
+        return *this = (*this / scl);
     } 
 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ operator *= ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    template<class E, 
-             typename std::enable_if<is_ScalarType<E>::value>::type*& =
-                 enabler>
-    RealDynamicVector& operator*=(const E& exp)
-    {
-        *this = DynamicVectorSclMul<RealDynamicVector>(exp, *this);
-        return *this;
-    } 
+    elem_t const& operator[](const std::size_t i) const {return values_[i];}
+    elem_t&       operator[](const std::size_t i)       {return values_[i];}
+    elem_t const& at(const std::size_t i) const {return values_.at(i);}
+    elem_t&       at(const std::size_t i)       {return values_.at(i);}
 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ operator /= ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    template<class E, 
-             typename std::enable_if<is_ScalarType<E>::value>::type*& =
-                 enabler>
-    RealDynamicVector& operator/=(const E& exp)
-    {
-        *this = DynamicVectorSclDiv<RealDynamicVector>(*this, exp);
-        return *this;
-    } 
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ member access ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    const double& operator[](const std::size_t i) const
-    {
-        return values_[i];
-    }
-
-    double& operator[](const std::size_t i)
-    {
-        return values_[i];
-    }
-
-    const double& at(const std::size_t i) const
-    {
-        return values_.at(i);
-    }
-
-    double& at(const std::size_t i)
-    {
-        return values_.at(i);
-    }
-
-    const std::size_t size() const
-    {
-        return values_.size();
-    }
+    std::size_t size() const {return values_.size();}
 
   private:
 
-    std::vector<double> values_;
+    std::vector<elem_t> values_;
 };
 
 }
