c7699a372afc4e3c02c43eb1610f5ed83fd3271a
ToruNiina <niina.toru.68u@gmail.com>
2016-07-20 10:53:09 +0900

    add dimension func for matrix

--- src/Dimension.hpp
+++ src/Dimension.hpp
@@ -1,6 +1,7 @@
 #ifndef AX_DIMENSION_FUNC_H
 #define AX_DIMENSION_FUNC_H
 #include "TypeTraits.hpp"
+#include <utility>
 
 namespace ax
 {
@@ -40,6 +41,76 @@ constexpr inline std::size_t dimension(const T_vexpr& vexpr)
 {
     //XXX: in vector_scalar_operation, l must be vector
     return dimension(vexpr.l_);
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Matrix ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+template <class T_mat, typename std::enable_if<
+              is_matrix_type<typename T_mat::tag>::value&&
+              is_static_dimension<T_mat::dim_col>::value>::type*& = enabler>
+constexpr inline std::size_t dimension_col(const T_mat&)
+{
+    return T_mat::dim_col;
+}
+
+template <class T_mat, typename std::enable_if<
+              is_matrix_type<typename T_mat::tag>::value&&
+              is_dynamic_dimension<T_mat::dim_col>::value>::type*& = enabler>
+constexpr inline std::size_t dimension_col(const T_mat& m)
+{
+    return m.size_col();
+}
+
+template <class T_mexpr, typename std::enable_if<
+              is_exactly_matrix_expr<typename T_mexpr::tag>::value&&
+              is_static_dimension<T_mexpr::dim_col>::value>::type*& = enabler>
+constexpr inline std::size_t dimension_col(const T_mexpr&)
+{
+    return T_mexpr::dim_col;
+}
+
+template <class T_mexpr, typename std::enable_if<
+              is_exactly_matrix_expr<typename T_mexpr::tag>::value&&
+              is_dynamic_dimension<T_mexpr::dim>::value>::type*& = enabler>
+constexpr inline std::size_t dimension_col(const T_mexpr& mexpr)
+{
+    return dimension_col(mexpr.l_);
+}
+
+template <class T_mat, typename std::enable_if<
+              is_matrix_type<typename T_mat::tag>::value&&
+              is_static_dimension<T_mat::dim_row>::value>::type*& = enabler>
+constexpr inline std::size_t dimension_row(const T_mat&)
+{
+    return T_mat::dim_row;
+}
+
+template <class T_mat, typename std::enable_if<
+              is_matrix_type<typename T_mat::tag>::value&&
+              is_dynamic_dimension<T_mat::dim_row>::value>::type*& = enabler>
+constexpr inline std::size_t dimension_row(const T_mat& m)
+{
+    return m.size_row();
+}
+
+template <class T_mexpr, typename std::enable_if<
+              is_exactly_matrix_expr<typename T_mexpr::tag>::value&&
+              is_static_dimension<T_mexpr::dim_row>::value>::type*& = enabler>
+constexpr inline std::size_t dimension_row(const T_mexpr&){return T_mexpr::dim_row;}
+
+template <class T_mexpr, typename std::enable_if<
+              is_exactly_matrix_expr<typename T_mexpr::tag>::value&&
+              is_dynamic_dimension<T_mexpr::dim>::value>::type*& = enabler>
+constexpr inline std::size_t dimension_row(const T_mexpr& mexpr)
+{
+    return dimension_row(mexpr.l_);
+}
+
+template <class T_mat, typename std::enable_if<
+              is_matrix_type<typename T_mat::tag>::value&&
+              is_static_dimension<T_mat::dim_row>::value>::type*& = enabler>
+constexpr inline std::pair<std::size_t, std::size_t> dimension(const T_mat& m)
+{
+    return std::make_pair(dimension_row(m), dimension_col(m));
 }
 
 }// ax
