74d6c063298032a336e3ad4b26a6a518eff03155
ToruNiina <niina.toru.68u@gmail.com>
2016-07-20 15:10:02 +0900

    add stream operator>>

--- src/io.hpp
+++ src/io.hpp
@@ -15,6 +15,15 @@ std::ostream& operator<<(std::ostream& os, const T_vec& vec)
     return os;
 }
 
+template<class T_vec, typename std::enable_if<
+    is_vector_expression<typename T_vec::tag>::value>::type*& = enabler>
+std::istream& operator>>(std::istream& is, T_vec& vec)
+{
+    for(std::size_t i = 0; i<dimension(vec); ++i)
+        is >> vec[i];
+    return is;
+}
+
 // template<class V,
 //          typename std::enable_if<
 //                  is_AVXVectorExpression<typename V::value_trait>::value
@@ -38,6 +47,16 @@ std::ostream& operator<<(std::ostream& os, const T_mat& mat)
         std::cout << std::endl;
     }
     return os;
+}
+
+template<class T_mat, typename std::enable_if<
+    is_matrix_expression<typename T_mat::tag>::value>::type*& = enabler>
+std::istream& operator>>(std::istream& is, T_mat& mat)
+{
+    for(std::size_t i = 0; i<dimension_row(mat); ++i)
+        for(std::size_t j = 0; j<dimension_col(mat); ++j)
+            is >> mat(i,j);
+    return is;
 }
 
 }
