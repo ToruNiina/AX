0dfca381c6522214bc8d7275af0fdadaf694c828
ToruNiina <niina.toru.68u@gmail.com>
2016-07-21 15:01:30 +0900

    add InverseMatrix

--- src/InverseMatrix.hpp
+++ src/InverseMatrix.hpp
@@ -6,70 +6,69 @@ namespace ax
 {
 
 // for 2x2 dimentional
-template<class L, typename std::enable_if<
-             is_MatrixExpression<typename L::value_trait>::value&&
-             is_SameSize<L::col, L::row>::value&&
-             is_SameSize<L::col, 2>::value
-             >::type*& = enabler>
-const double determinant(const L& lhs)
+template<class T_mat, typename std::enable_if<
+    is_matrix_expression<typename T_mat::tag>::value&&
+    is_same_dimension<T_mat::dim_col, T_mat::dim_row>::value&&
+    is_same_dimension<T_mat::dim_col, 2>::value>::type*& = enabler>
+inline typename T_mat::elem_t
+determinant(const T_mat& mat)
 {
-    return lhs(0,0) * lhs(1,1) - lhs(1,0) * lhs(0,1);
+    return mat(0,0) * mat(1,1) - mat(1,0) * mat(0,1);
 }
 
 // for 3x3 dimentional
-template<class L, typename std::enable_if<
-             is_MatrixExpression<typename L::value_trait>::value&&
-             is_SameSize<L::col, L::row>::value&&
-             is_SameSize<L::col, 3>::value
-             >::type*& = enabler>
-const double determinant(const L& lhs)
+template<class T_mat, typename std::enable_if<
+    is_matrix_expression<typename T_mat::tag>::value&&
+    is_same_dimension<T_mat::dim_col, T_mat::dim_row>::value&&
+    is_same_dimension<T_mat::dim_col, 3>::value>::type*& = enabler>
+inline typename T_mat::elem_t
+determinant(const T_mat& mat)
 {
-    return lhs(0,0) * lhs(1,1) * lhs(2,2) +
-           lhs(1,0) * lhs(2,1) * lhs(0,2) +
-           lhs(2,0) * lhs(0,1) * lhs(1,2) - 
-           lhs(0,0) * lhs(2,1) * lhs(1,2) - 
-           lhs(2,0) * lhs(1,1) * lhs(0,2) - 
-           lhs(1,0) * lhs(0,1) * lhs(2,2);
+    return mat(0,0) * mat(1,1) * mat(2,2) +
+           mat(1,0) * mat(2,1) * mat(0,2) +
+           mat(2,0) * mat(0,1) * mat(1,2) - 
+           mat(0,0) * mat(2,1) * mat(1,2) - 
+           mat(2,0) * mat(1,1) * mat(0,2) - 
+           mat(1,0) * mat(0,1) * mat(2,2);
 }
 
-template<class L, typename std::enable_if<
-             is_MatrixExpression<typename L::value_trait>::value&&
-             is_SameSize<L::col, L::row>::value&&
-             is_SameSize<L::col, 2>::value
-             >::type*& = enabler>
-RealMatrix<L::row, L::col> inverse(const L& lhs)
+template<class T_mat, typename std::enable_if<
+    is_matrix_expression<typename T_mat::tag>::value&&
+    is_same_dimension<T_mat::dim_col, T_mat::dim_row>::value&&
+    is_same_dimension<T_mat::dim_col, 2>::value>::type*& = enabler>
+Matrix<typename T_mat::elem_t, T_mat::dim_row, T_mat::dim_col>
+inverse(const T_mat& mat)
 {
-    const double det_inv = 1e0 / determinant(lhs);
-    RealMatrix<L::row, L::col> inv;
-    inv(0,0) =  det_inv * lhs(1,1);
-    inv(1,1) =  det_inv * lhs(0,0);
-    inv(0,1) = -det_inv * lhs(0,1);
-    inv(1,0) = -det_inv * lhs(1,0);
+    const auto det_inv = 1e0 / determinant(mat);
+    Matrix<typename T_mat::elem_t, T_mat::dim_row, T_mat::dim_col> inv;
+    inv(0,0) =  det_inv * mat(1,1);
+    inv(1,1) =  det_inv * mat(0,0);
+    inv(0,1) = -det_inv * mat(0,1);
+    inv(1,0) = -det_inv * mat(1,0);
     return inv;
 }
 
-template<class L, typename std::enable_if<
-             is_MatrixExpression<typename L::value_trait>::value&&
-             is_SameSize<L::col, L::row>::value&&
-             is_SameSize<L::col, 3>::value
-             >::type*& = enabler>
-RealMatrix<L::row, L::col> inverse(const L& lhs_)
+template<class T_mat, typename std::enable_if<
+    is_matrix_expression<typename T_mat::tag>::value&&
+    is_same_dimension<T_mat::dim_col, T_mat::dim_row>::value&&
+    is_same_dimension<T_mat::dim_col, 3>::value>::type*& = enabler>
+Matrix<typename T_mat::elem_t, T_mat::dim_row, T_mat::dim_col>
+inverse(const T_mat& mat)
 {
-    const L lhs = lhs_;
-    const double det_inv = 1e0 / determinant(lhs);
+    const auto det_inv = 1e0 / determinant(mat);
 
-    RealMatrix<L::row, L::col> inv;
-    inv(0,0) = det_inv * (lhs(1,1) * lhs(2,2) - lhs(1,2) * lhs(2,1));
-    inv(1,1) = det_inv * (lhs(0,0) * lhs(2,2) - lhs(0,2) * lhs(2,0));
-    inv(2,2) = det_inv * (lhs(0,0) * lhs(1,1) - lhs(0,1) * lhs(1,0));
+    Matrix<typename T_mat::elem_t, T_mat::dim_row, T_mat::dim_col> inv;
+    inv(0,0) = det_inv * (mat(1,1) * mat(2,2) - mat(1,2) * mat(2,1));
+    inv(1,1) = det_inv * (mat(0,0) * mat(2,2) - mat(0,2) * mat(2,0));
+    inv(2,2) = det_inv * (mat(0,0) * mat(1,1) - mat(0,1) * mat(1,0));
 
-    inv(0,1) = det_inv * (lhs(0,2) * lhs(2,1) - lhs(0,1) * lhs(2,2));
-    inv(0,2) = det_inv * (lhs(0,1) * lhs(1,2) - lhs(0,2) * lhs(1,1));
-    inv(1,2) = det_inv * (lhs(0,2) * lhs(1,0) - lhs(0,0) * lhs(1,2));
+    inv(0,1) = det_inv * (mat(0,2) * mat(2,1) - mat(0,1) * mat(2,2));
+    inv(0,2) = det_inv * (mat(0,1) * mat(1,2) - mat(0,2) * mat(1,1));
+    inv(1,2) = det_inv * (mat(0,2) * mat(1,0) - mat(0,0) * mat(1,2));
 
-    inv(1,0) = det_inv * (lhs(1,2) * lhs(2,0) - lhs(1,0) * lhs(2,2));
-    inv(2,0) = det_inv * (lhs(1,0) * lhs(2,1) - lhs(2,0) * lhs(1,1));
-    inv(2,1) = det_inv * (lhs(2,0) * lhs(0,1) - lhs(0,0) * lhs(2,1));
+    inv(1,0) = det_inv * (mat(1,2) * mat(2,0) - mat(1,0) * mat(2,2));
+    inv(2,0) = det_inv * (mat(1,0) * mat(2,1) - mat(2,0) * mat(1,1));
+    inv(2,1) = det_inv * (mat(2,0) * mat(0,1) - mat(0,0) * mat(2,1));
     return inv;
 }
 
