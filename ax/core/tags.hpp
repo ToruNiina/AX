a9bd6accb879f56b1834db84336513558be2ef2f
ToruNiina <niina.toru.68u@gmail.com>
2016-12-11 14:44:17 +0900

    add tags

---
+++ ax/core/tags.hpp
@@ -0,0 +1,14 @@
+#ifndef AX_TAGS
+#define AX_TAGS
+
+namespace ax
+{
+
+struct matrix_tag{};
+struct vector_tag{};
+struct expression_tag{};
+struct operator_tag{};
+
+} // ax
+
+#endif /* AX_TAGS */
